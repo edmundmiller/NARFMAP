03fd2563dbeeb3fd390c4fdb8664cff3e8b4b9f5
Edmund Miller <git@edmundmiller.dev>
2024-01-09 12:20:35 -0600

    build: Add some environment variables

--- crates.nix
+++ crates.nix
@@ -11,13 +11,25 @@
     nci.projects.${crateName}.path = ./.;
     # configure crates
     nci.crates.${crateName} = {
-      drvConfig.mkDerivation = {
-        buildInputs = [
-          pkgs.boost
-          pkgs.gnumake
-          pkgs.gtest
-          pkgs.zlib
-        ];
+      drvConfig = {
+        mkDerivation = {
+          buildInputs = [
+            pkgs.boost
+            pkgs.gnumake
+            pkgs.gtest
+            pkgs.zlib
+          ];
+        };
+
+        env = {
+          BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
+          BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
+          GTEST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.gtest}/include";
+          GTEST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.gtest}/lib";
+          GTEST_ROOT = "${pkgs.gtest}";
+          LD_LIBRARY_PATH = "${pkgs.lib.getLib pkgs.gtest}/lib";
+          LIBCLANG_PATH = "${pkgs.lib.getLib pkgs.llvmPackages.libclang.lib}/lib";
+        };
       };
     };
   };
