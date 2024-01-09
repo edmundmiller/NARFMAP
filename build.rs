45c50face44f07c5b93e5758955ff83c3d4b0590
Edmund Miller <git@edmundmiller.dev>
2024-01-09 10:23:13 -0600

    build: Include boost

--- build.rs
+++ build.rs
@@ -1,10 +1,14 @@
+use std::path::Path;
+
 fn main() -> miette::Result<()> {
-    let include_dir = Some(std::path::Path::new("src/include/"));
+    let include_dir = Path::new("src/include/");
+    let boost_includedir = Path::new(env!("BOOST_INCLUDEDIR"));
 
     let path = std::path::PathBuf::from("src"); // include path
     let mut b = autocxx_build::Builder::new("src/main.rs", &[&path]).build()?;
     b.std("c++17")
-        .includes(include_dir)
+        .include(include_dir)
+        .include(boost_includedir)
         .file("src/dragen-os.cpp")
         .compile("dragen-os"); // arbitrary library name, pick anything
     println!("cargo:rerun-if-changed=src/main.rs");
