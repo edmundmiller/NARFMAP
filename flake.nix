73efaf62380303c8ad721005aa1fd03e328067a1
Edmund Miller <git@edmundmiller.dev>
2024-06-24 21:21:55 -0500

    build: nix flake init --template github:the-nix-way/dev-templates#zig

--- flake.nix
+++ flake.nix
@@ -1,125 +1,69 @@
 {
-  description = "Description for the project";
+  description = "A Nix-flake-based Zig development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nci.url = "github:yusdacra/nix-cargo-integration";
-    nix2container.url = "github:nlewo/nix2container";
-    nix2container.inputs.nixpkgs.follows = "nixpkgs";
-    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [zig];
+      };
+    });
+
+    packages = forEachSupportedSystem ({pkgs}: {
+      dragen = pkgs.stdenv.mkDerivation {
+        pname = "NARFMAP";
+        version = "1.4.0";
+
+        src = ./.;
+
+        buildInputs = [
+          pkgs.boost
+          pkgs.gnumake
+          pkgs.gtest
+          pkgs.zlib
+        ];
+
+        buildPhase = ''
+          make
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/release/compare $out/bin
+          cp build/release/dragen-os $out/bin
+        '';
+
+        env = {
+          BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
+          BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
+          GTEST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.gtest}/include";
+          GTEST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.gtest}/lib";
+          GTEST_ROOT = "${pkgs.gtest}";
+          LD_LIBRARY_PATH = "${pkgs.lib.getLib pkgs.gtest}/lib";
+          LIBCLANG_PATH = "${pkgs.lib.getLib pkgs.llvmPackages.libclang.lib}/lib";
+        };
+
+        meta = with pkgs.lib; {
+          homepage = "https://github.com/nixvital/nix-based-cpp-starterkit";
+          description = ''
+            A template for Nix based C++ project setup.";
+          '';
+          licencse = licenses.mit;
+          platforms = with platforms; linux ++ darwin;
+          maintainers = [maintainers.breakds];
+        };
+      };
+    });
   };
-
-  outputs = inputs @ {
-    flake-parts,
-    nci,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.nci.flakeModule
-        inputs.treefmt-nix.flakeModule
-        ./crates.nix
-      ];
-      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: let
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        crateName = cargoToml.package.name;
-        crateOutputs = config.nci.outputs.${crateName};
-      in {
-        packages.default = crateOutputs.packages.release;
-
-        devShells.default = crateOutputs.devShell;
-
-        packages.dragen = pkgs.stdenv.mkDerivation {
-          pname = "NARFMAP";
-          version = cargoToml.package.version;
-
-          src = ./.;
-
-          buildInputs = [
-            pkgs.boost
-            pkgs.gnumake
-            pkgs.gtest
-            pkgs.zlib
-          ];
-
-          buildPhase = ''
-            make
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/release/compare $out/bin
-            cp build/release/dragen-os $out/bin
-          '';
-
-          env = {
-            BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
-            BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
-            GTEST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.gtest}/include";
-            GTEST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.gtest}/lib";
-            GTEST_ROOT = "${pkgs.gtest}";
-            LD_LIBRARY_PATH = "${pkgs.lib.getLib pkgs.gtest}/lib";
-            LIBCLANG_PATH = "${pkgs.lib.getLib pkgs.llvmPackages.libclang.lib}/lib";
-          };
-
-          meta = with pkgs.lib; {
-            homepage = "https://github.com/nixvital/nix-based-cpp-starterkit";
-            description = ''
-              A template for Nix based C++ project setup.";
-            '';
-            licencse = licenses.mit;
-            platforms = with platforms; linux ++ darwin;
-            maintainers = [maintainers.breakds];
-          };
-        };
-
-        # FIXME
-        # devShells.dragen = {
-        #   name = "NARFMAP";
-
-        #   imports = [
-        #   ];
-
-        #   packages = [
-        #     pkgs.boost
-        #     pkgs.gnumake
-        #     pkgs.gtest
-        #     pkgs.zlib
-        #   ];
-        #   env = {
-        #     BOOST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.boost}/include";
-        #     BOOST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.boost}/lib";
-        #     GTEST_INCLUDEDIR = "${pkgs.lib.getDev pkgs.gtest}/include";
-        #     GTEST_LIBRARYDIR = "${pkgs.lib.getLib pkgs.gtest}/lib";
-        #     GTEST_ROOT = "${pkgs.gtest}";
-        #     LD_LIBRARY_PATH = "${pkgs.lib.getLib pkgs.gtest}/lib";
-        #   };
-        # };
-
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-
-          programs.alejandra.enable = true;
-          programs.clang-format.enable = true;
-          programs.deadnix.enable = true;
-          programs.prettier.enable = true;
-          programs.prettier.settings = {
-            editorconfig = true;
-            embeddedLanguageFormatting = "auto";
-          };
-        };
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-      };
-    };
 }
