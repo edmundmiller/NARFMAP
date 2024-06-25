d008008142e56a240dc6a39b3ced25a2d8f8d764
Edmund Miller <git@edmundmiller.dev>
2024-06-25 11:49:36 -0500

    build: Add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
       flake = {
         # TODO Put your original flake attributes here.
       };
@@ -17,7 +21,6 @@
       perSystem = {
         config,
         pkgs,
-        system,
         ...
       }: {
         #
@@ -87,6 +90,18 @@
             licencse = licenses.mit;
             platforms = with platforms; linux ++ darwin;
             maintainers = [maintainers.breakds];
+          };
+        };
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+
+          programs.alejandra.enable = true;
+          programs.clang-format.enable = true;
+          programs.deadnix.enable = true;
+          programs.prettier.enable = true;
+          programs.prettier.settings = {
+            editorconfig = true;
+            embeddedLanguageFormatting = "auto";
           };
         };
       };
